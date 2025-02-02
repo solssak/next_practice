cb20c26c4c6f175695e16893781cce7a858df47e
solssak <gswoong1012@gmail.com>
2025-02-02 10:39:17 +0900

    feat: set project

---
+++ src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,26 @@
+import BookItem from '@/components/book-item';
+import style from './page.module.css';
+import books from '@/mock/books.json';
+
+export default async function Home() {
+  const response = await fetch(`http://localhost:12345/book`);
+  const allBooks = await response.json();
+  console.log(allBooks);
+
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {allBooks.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {allBooks.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
+  );
+}
